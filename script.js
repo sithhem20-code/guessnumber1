443a45ece8202b0f27b569fc64ed731f7b48886b
sithhem20-code <sithhem20@gmail.com>
2026-07-21 11:10:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-// Generate a random number between 1 and 100
+
 let secretNumber = Math.floor(Math.random() * 100) + 1;
 let attempts = 0;
 
-// Check the player's guess
 function checkGuess() {
 
     let guessInput = document.getElementById("guessInput");
     let guess = Number(guessInput.value);
     let message = document.getElementById("message");
 
-    // Check if the input is empty
     if (guessInput.value === "") {
         message.innerText = "Please enter a number!";
         guessInput.focus();
         return;
     }
 
-    // Check if the number is between 1 and 100
     if (guess < 1 || guess > 100) {
         message.innerText = "Please enter a number between 1 and 100!";
         guessInput.value = "";
@@ -29,25 +26,19 @@ function checkGuess() {
 
     if (guess < secretNumber) {
         message.innerText = "📉 Too Low!";
-        guessInput.value = "";
-        guessInput.focus();
     }
     else if (guess > secretNumber) {
         message.innerText = "📈 Too High!";
-        guessInput.value = "";
-        guessInput.focus();
     }
     else {
         message.innerText =
-            "🎉 Congratulations! You guessed the number in " +
-            attempts +
-            " attempt(s)!";
-
-        guessInput.value = "";
+            `🎉 Congratulations! You guessed the correct number: ${secretNumber} in ${attempts} attempt(s)!`;
     }
+
+    guessInput.value = "";
+    guessInput.focus();
 }
 
-// Start a new game
 function restartGame() {
     secretNumber = Math.floor(Math.random() * 100) + 1;
     attempts = 0;
